7ac3358b8a972b5f30110f753de50a9df857315d
KaluginaMarina <marin.kalugin@gmail.com>
2019-01-18 19:49:35 +0300

    react router

--- robogit/front/src/reducers/index.js
+++ robogit/front/src/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import products from './products';
+import platforms from  './platforms'
 
 export default combineReducers({
     routing: routerReducer,
-    products
+    products,
+    platforms
 })
