761251d261cd448d2a6a433eb12ddc43c6e1c4e9
ivankayukoff <ivankayukoff@gmail.com>
2019-01-20 14:37:12 +0300

    add commented docker config

--- robogit/src/main/kotlin/org/robogit/config/DatabaseConfig.kt
+++ robogit/src/main/kotlin/org/robogit/config/DatabaseConfig.kt
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
-@EnableJpaRepositories(basePackages = arrayOf("org.robogit.repository"))
+@EnableJpaRepositories(basePackages = ["org.robogit.repository"])
 @Configuration
 @EnableTransactionManagement
 open class DatabaseConfig {
   companion object {
-    // const val SCHEMA_NAME = "s244707" // helios config
-    const val SCHEMA_NAME = "public" // docker config
+    const val SCHEMA_NAME = "s244707" // Marina's config
+    // const val SCHEMA_NAME = "public" // docker config
   }
 
 }
