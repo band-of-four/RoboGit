d45d961906d1b466ad884c54cc0c8c2a507956c1
KaluginaMarina <marin.kalugin@gmail.com>
2019-01-24 20:39:25 +0300

    фронт small fixes :D

--- robogit/front/src/index.js
+++ robogit/front/src/index.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import {syncHistoryWithStore} from 'react-router-redux';
-import {Router, Route, browserHistory} from 'react-router';
-import {Provider} from 'react-redux';
-import { Switch } from 'react-router-dom';
+import './main.css'
 
-import reducers from 'reducers';
-import Layout from 'containers/layoyt';
-import Products from 'containers/products';
-import Platforms from 'containers/platforms';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
 
-import { createBrowserHistory } from 'history';
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Informations from 'containers/informations'
+import Information from 'containers/information'
+import Basket from 'containers/basket'
 
 const store = createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunk)
-));
-const history = syncHistoryWithStore(createBrowserHistory(), store);
+  applyMiddleware(thunk)
+))
 
+const history = syncHistoryWithStore(browserHistory, store)
+console.log("!")
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Layout>
-                <Switch>
-                    <Route path='/' component={Products} />
-                    <Route path='/platforms' component={Platforms} />
-                </Switch>
-            </Layout>
-        </Router>
-    </Provider>,
-    document.getElementById('app')
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+        <Route path='/' component={Informations}/>
+        <Route path='/categories/:id' component={Informations}/>
+      </Route>
+      <Route path='/informations/:id' component={Information}/>
+      <Route path='/basket' component={Basket} />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
